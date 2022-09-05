3a81c03706b25d6455c926106a8e6b81335deb6f
Jake Cook <jake.w.cook@protonmail.com>
2022-09-04 21:09:44 -0600

    Project init and starting wireframe

---
+++ muchtodo-web/src/App.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import ListPage from "./components/ListPage.vue";
+
+export default defineComponent({
+  name: "MuchTodo",
+  components: {
+    ListPage,
+  },
+  data() {
+    return {
+      tasklists: [],
+    };
+  },
+});
+</script>
+
+<template>
+  <div id="Title">
+    <title>MuchTodo: List and Note Management</title>
+    <h1 id="master-title">MuchTodo: List and Note Management</h1>
+  </div>
+  <div id="MuchTodoRoot">
+    <h2>Available Tasks:</h2>
+    <ListPage />
+  </div>
+</template>
+
+<style>
+#master-title {
+  color: rgba(0, 68, 255, 0.795);
+  /* align-content: center; */
+  text-align: center;
+}
+#muchtodo-root {
+  text-align: center;
+  align-content: center;
+  align-items: center;
+}
+</style>
---
+++ muchtodo-web/src/components/ListPage.vue
@@ -0,0 +1,25 @@
+<script lang="js">
+
+let id = 0;
+
+export default {
+  data() {
+    return {
+      taskitems: [
+        {id: id++, 'text': 'Task item 1', done: false, deleted: false}
+    ],
+      num: 1
+    }
+  }
+}
+</script>
+
+<template>
+  <p v-for="task in taskitems" :key="task.text">{{ task.text }}</p>
+</template>
+
+<style>
+.temp {
+  color: red;
+}
+</style>
