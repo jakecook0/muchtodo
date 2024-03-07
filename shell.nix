892b98ed5bc3cdf1d6b0662eedf9073cddd51b24
jcookin <>
2024-03-06 19:24:30 -0700

    Fix version pins

--- shell.nix
+++ shell.nix
@@ -4,8 +4,7 @@ with pkgs;
 pkgs.mkShell {
 
   nativeBuildInputs = with pkgs.buildPackages; [
-    pkgs.go #-1.21.6  # nix-env -qP --available go
-    pkgs.gopls-0.14.2
+    pkgs.go_1_21 # 1.21.6  # nix-env -qP --available go
+    pkgs.gopls # 0.14.2
   ];
-
 }
